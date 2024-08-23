28a740107d97aa0fa3490f0f8aeebf43416d7def
super-boost-fps <muonremote@gmail.com>
2024-08-23 11:49:44 +0700

    Update kick.lua

--- kick.lua
+++ kick.lua
@@ -1,4 +1,10 @@
-script.Parent.Touched:Connect(function(hit)
-	local player = game.Players:GetPlayerFromCharacter(hit.Parent)
-	player:Kick("Invalid key or wrong HWID. Please contact #adromix._.")
-end)
+local function kickPlayer(reason)
+    local player = game.Players.LocalPlayer
+    if player then
+        player:Kick(reason)
+    else
+        warn("No player found to kick.")
+    end
+end
+
+kickPlayer("Invalid key or wrong HWID. Please contact #adromix._.")
